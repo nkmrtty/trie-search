be1e407b572edee0307943028a2e0149ff842f07
joein <panchuk.george@outlook.com>
2021-09-17 23:16:23 +0300

    new: splitter regexp support

--- tests/test_triesearch.py
+++ tests/test_triesearch.py
@@ -4,19 +4,31 @@ from trie_search.trie import TrieSearch
 
 class TestTrieSearch(TestCase):
     def test_search_all_patterns(self):
-        patterns = ['new york', 'york city', 'new york city']
-        text = 'new york city'
-        answers = {'new york': 0, 'york city': 4, 'new york city': 0}
+        patterns = ["new york", "york city", "new york city"]
+        text = "new york city"
+        answers = {"new york": 0, "york city": 4, "new york city": 0}
         trie = TrieSearch(patterns=patterns)
-
         for ptn, idx in trie.search_all_patterns(text):
             self.assertIn(ptn, answers)
             self.assertEqual(idx, answers[ptn])
 
+    def test_search_regexp_splitter(self):
+        patterns = [
+            "new york",
+            "york city",
+        ]
+        text = "new york, york city and some new york city here"
+        splitter = f"[\w\d]+"
+        answers = {"new york": {0, 29}, "york city": {10, 33}}
+        trie = TrieSearch(patterns=patterns, splitter=splitter)
+        for ptn, idx in trie.search_all_patterns(text):
+            self.assertIn(ptn, answers)
+            self.assertIn(idx, answers[ptn])
+
     def test_search_longest_patterns(self):
-        patterns = ['new york', 'york city', 'new york city']
-        text = 'new york city'
-        answers = {'new york city': 0}
+        patterns = ["new york", "york city", "new york city"]
+        text = "new york city"
+        answers = {"new york city": 0}
         trie = TrieSearch(patterns=patterns)
 
         for ptn, idx in trie.search_longest_patterns(text):
@@ -24,20 +36,20 @@ class TestTrieSearch(TestCase):
             self.assertEqual(idx, answers[ptn])
 
     def test_search_all_patterns_jp(self):
-        patterns = ['ニューヨーク', 'ヨークシティ', 'ニューヨークシティ']
-        text = 'ニューヨークシティ'
-        answers = {'ニューヨーク': 0, 'ヨークシティ': 3, 'ニューヨークシティ': 0}
-        trie = TrieSearch(patterns=patterns, splitter='')
+        patterns = ["ニューヨーク", "ヨークシティ", "ニューヨークシティ"]
+        text = "ニューヨークシティ"
+        answers = {"ニューヨーク": 0, "ヨークシティ": 3, "ニューヨークシティ": 0}
+        trie = TrieSearch(patterns=patterns, splitter="")
 
         for ptn, idx in trie.search_all_patterns(text):
             self.assertIn(ptn, answers)
             self.assertEqual(idx, answers[ptn])
 
     def test_search_longest_patterns_jp(self):
-        patterns = ['ニューヨーク', 'ヨークシティ', 'ニューヨークシティ']
-        text = 'ニューヨークシティ'
-        answers = {'ニューヨークシティ': 0}
-        trie = TrieSearch(patterns=patterns, splitter='')
+        patterns = ["ニューヨーク", "ヨークシティ", "ニューヨークシティ"]
+        text = "ニューヨークシティ"
+        answers = {"ニューヨークシティ": 0}
+        trie = TrieSearch(patterns=patterns, splitter="")
 
         for ptn, idx in trie.search_longest_patterns(text):
             self.assertIn(ptn, answers)
